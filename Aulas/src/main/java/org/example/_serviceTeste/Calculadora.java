4494c2c2d92e5ae5b76c6bb17469e17f4916fb42
Samuel Hermany <samuelhermany@hotmail.com>
2025-04-19 20:50:09 -0300

    feat: Adicioando pasta co mas Aulas até aula 02 - 17/04/25

---
+++ Aulas/src/main/java/org/example/_serviceTeste/Calculadora.java
@@ -0,0 +1,11 @@
+package org.example._serviceTeste;
+
+public class Calculadora {
+    public int somar(int a, int b){
+        return a + b;
+    }
+
+    public int dividir(int a, int b) {
+        return a / b;
+    }
+}
